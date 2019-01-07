1648e127307029969134d7fe748e22ef782cff37
David Varga <varga.david@duvinet.hu>
2019-01-07 14:51:13 +0100

    show_pos: use dedicated mysql connection

--- res/show_pos.php
+++ res/show_pos.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once('config.php');
+require_once('db.php');
 
 $message = "";
-$pos_x = "";
-$pos_y = "";
 
     if ($_POST && isset($_POST['pos']))
         {
@@ -15,33 +13,19 @@ $pos_y = "";
         $param = "120";
         }
 
-    $connection = mysqli_connect($db_host,$db_user,$db_pass);
-    if (!$connection)
-        {
-        die("Database connection failed: " . mysqli_error($connection));
-        }
-
-    $db_select = mysqli_select_db($connection,$db_name);
-    if (!$db_select)
-        {
-        die("Database selection failed: " . mysqli_error($connection));
-        }
-
     $sql = "SELECT x, y, time_rec
             FROM position_list
             WHERE pos_id = '" . $param . "'";
 
-    $result = mysqli_query($connection,$sql);
+    $result = db_select($sql);
     if (!$result)
         {
-        die("Database query failed: " . mysqli_error($connection));
+            $message .= "Position not found.\n";
         }
 
     if ($row = mysqli_fetch_array($result))
         {
-        $pos_x = $row["x"];
-        $pos_y = $row["y"];
-        $message .= "POSITION: " . $param . " [" . $pos_x . "," . $pos_y . "] TIME: " . $row['time_rec'] . " \n";
+        $message .= "POSITION: " . $param . " [" . $row['x'] . "," . $row['y'] . "] TIME: " . $row['time_rec'] . " \n";
 
         $sql = "SELECT position_data.signal, position_data.mean, position_data.std_dev, monitor_list.ip
                 FROM position_data
@@ -50,25 +34,17 @@ $pos_y = "";
                 WHERE position_data.pos_id = '" . $param . "'
                 ORDER BY position_data.mean";
 
-        $result = mysqli_query($connection,$sql);
-        if (!$result)
+        $result2 = db_select($sql);
+
+        while ($row2 = mysqli_fetch_array($result2))
             {
-            die("Database query failed: " . mysqli_error($connection));
+            $message .= "MON: " . $row2['ip'] . " SIG: -" . $row2['signal'] . " dBm   MEAN: -" . $row2['mean'] . " dBm  DEV: " . $row2['std_dev'] . " \n";
             }
-        while ($row = mysqli_fetch_array($result))
-            {
-            $message .= "MON: " . $row['ip'] . " SIG: -" . $row['signal'] . " dBm   MEAN: -" . $row['mean'] . " dBm  DEV: " . $row['std_dev'] . " \n";
-            }
-        }
-    else
-        {
-        $message .= "Position not found.\n";
+        mysqli_free_result($result2);
         }
 
     mysqli_free_result($result);
-    mysqli_close($connection);
 
-    echo json_encode(array('x'=>$pos_x,'y'=>$pos_y,'message'=>nl2br($message)));
+    echo json_encode(array('x'=>$row['x'],'y'=>$row['y'],'message'=>nl2br($message)));
 
 ?>
-
