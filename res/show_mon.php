5a3f03846778db8f573b3546e84d0a7991907349
David Varga <varga.david@duvinet.hu>
2019-01-07 14:11:30 +0100

    show_mon: use dedicated mysql connection

--- res/show_mon.php
+++ res/show_mon.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('config.php');
+require_once('db.php');
 
 $rows = array();
 $message = "";
@@ -14,29 +14,17 @@ $message = "";
         $param = "b0487ac5f0be";
         }
 
-    $connection = mysqli_connect($db_host,$db_user,$db_pass);
-    if (!$connection)
-    {
-        die("Database connection failed: " . mysqli_error($connection));
-    }
-
-    $db_select = mysqli_select_db($connection,$db_name);
-    if (!$db_select)
-    {
-        die("Database selection failed: " . mysqli_error($connection));
-    }
-
     $sql = "SELECT position_data.signal, position_list.x, position_list.y
             FROM position_data
             LEFT JOIN position_list
             ON position_data.pos_id = position_list.pos_id
             WHERE position_data.mon_id='" . $param . "'";
 
-    $result = mysqli_query($connection,$sql);
+    $result = db_select($sql);
+
     if (!$result)
     {
         $message .= "Signals not received.\n";
-        die("Database query failed: " . mysqli_error($connection));
     }
 
     while ($row = mysqli_fetch_array($result))
@@ -45,7 +33,6 @@ $message = "";
         }
 
     mysqli_free_result($result);
-    mysqli_close($connection);
 
     $message .= json_encode($rows);
 
