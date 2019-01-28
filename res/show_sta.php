60731ee9a27d3c2a05458e4beeafed8d619631b6
David Varga <varga.david@duvinet.hu>
2019-01-28 13:30:27 +0100

    show_all, show_sta: print monitor name instead of ip

--- res/show_sta.php
+++ res/show_sta.php
@@ -27,17 +27,17 @@ $message = "";
         {
         $message .= "STATION: " . $row['sta_id'] . " CHANNEL: " . $row['channel'] . " \n";
 
-        $sql = "SELECT monitor_data.ip, station_data.signal, station_data.time_rcv
+        $sql = "SELECT monitor_list.name, station_data.signal, station_data.time_rcv
                             FROM station_data
-                            LEFT JOIN monitor_data
-                            ON station_data.mon_id = monitor_data.mon_id
+                            LEFT JOIN monitor_list
+                            ON station_data.mon_id = monitor_list.mon_id
                             WHERE station_data.sta_id = '" . $param . "'";
 
         $result2 = db_select($sql);
 
         while ($row2 = mysqli_fetch_array($result2))
             {
-            $message .= "MONITOR: " . $row2['ip'] . " SIGNAL: -" . $row2['signal'] . " dBm  TIME: " . $row2['time_rcv'] . " \n";
+            $message .= "MONITOR: " . $row2['name'] . " SIGNAL: -" . $row2['signal'] . " dBm  TIME: " . $row2['time_rcv'] . " \n";
             }
         mysqli_free_result($result2);
         }
