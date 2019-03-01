d9450686db41e455e4c7cf1092f57b5fe1c6c0ff
David Varga <varga.david@duvinet.hu>
2019-03-01 19:22:22 +0100

    cleanup index2.php

--- index2.php
+++ index2.php
@@ -16,10 +16,8 @@ else {
     $info_message .= "Map image not found!\n";
 }
 
-
 $mac = get_mac($ssh_host,$ssh_port,$ssh_user,$ssh_pass,$ssh_comm);
 $ip = $_SERVER['REMOTE_ADDR'];
-
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -29,39 +27,28 @@ $ip = $_SERVER['REMOTE_ADDR'];
         <title>Indoor Positioning System</title>
         <link type="text/css" rel="stylesheet" href="res/style.css" />
 
-        <script src="res/jquery.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="res/functions.js"></script>
 
-    <script>
-      var src_x;
-      var src_y;
-      var dest_x;
-      var dest_y;
-    </script>
-
+        <script>
+            var src_x;
+            var src_y;
+            var dest_x;
+            var dest_y;
+        </script>
     </head>
-    <body >
-
-    <div id="container">
-    <div id="maincontent">
-<?php
-    echo ('
-        <div id="navigate_map" style="background-image:url(' . $mapfile . '); width:' . $imagesize[0] . 'px; height:' . $imagesize[1] . 'px;">
-        <canvas id="navigate_canvas" width="' . $imagesize[0] . '" height="' . $imagesize[1] . '"></canvas>
-        <img src="img/rotpunkt.png" id="compare_punkt" style="position:absolute;z-index:2;left:0px;top:0px;visibility:hidden;">
-        <img src="img/rotpunkt.png" id="navigate_punkt" style="position:absolute;visibility:hidden;z-index:2;">
+    <body>
+        <div id="navigate_map" class="map" style="background-image:url('<?php echo $mapfile; ?>'); width:<?php echo $imagesize[0]; ?>px; height:<?php echo $imagesize[1]; ?>px;">
+            <canvas id="navigate_canvas" width="<?php echo $imagesize[0]; ?>" height="<?php echo $imagesize[1]; ?>" style="position: absolute;"></canvas>
+            <img src="img/rotpunkt.png" id="compare_punkt" style="position:absolute;z-index:2;left:0px;top:0px;visibility:hidden;">
+            <img src="img/rotpunkt.png" id="navigate_punkt" style="position:absolute;visibility:hidden;z-index:2;">
             <div id="nav_punkt"> </div>
         </div>
-        <script>
-            //drawpark();
-            compare2(\'comp_diff\',\'' . str_replace(":", "", $mac) . '\', src_x, src_y, limit);
-        </script>
-        <div id=compare_message> '
-        . (nl2br($message)) . '
+        <div id="compare_message" class="message">
+            <?php echo nl2br($message); ?>
         </div>
-    ');
-?>
-    </div>
-    </div>
+        <script>
+            compare2('comp_diff','<?php echo str_replace(":", "", $mac); ?>');
+        </script>
     </body>
 </html>
