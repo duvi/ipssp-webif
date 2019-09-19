b6ffddef5f807006d2aacb2d4588d30be779e9b3
David Varga <varga.david@duvinet.hu>
2019-09-19 13:22:20 +0200

    prepare for command line testing
    parse arguments as post variables

--- models/areas.php
+++ models/areas.php
@@ -1,4 +1,9 @@
 <?php
+/* if started from commandline, wrap parameters to $_POST and $_GET */
+if (!isset($_SERVER["HTTP_HOST"])) {
+    parse_str($argv[1], $_GET);
+    parse_str($argv[1], $_POST);
+}
 
 if ($_POST && isset($_POST['command'])) {
     $command = $_POST['command'];
